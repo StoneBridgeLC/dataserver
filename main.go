94c87f1f08dd1317cb78df7498a99a69c8cf8b69
elixter <ltw971@naver.com>
2021-04-07 18:58:20 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	db, err := sql.Open("mysql", "root:971216@tcp(127.0.0.1:3306/postanalyzer")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("show tabales")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var table string
+
+	for rows.Next() {
+		err := rows.Scan(&table)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(table)
+	}
+}
